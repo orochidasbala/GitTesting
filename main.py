a287ca5eddd80a8e4ab6291aa93c43ca1ddb9908
Dothon <souvenirhallow@gmail.com>
2022-03-05 11:10:19 +0630

    complete code

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
+from operator import truediv
+from traceback import print_tb
+
+
 name = input("Username : ")
 age = int(input('Age : '))
-email = input("Email : ")
 
-print(f"Hello {name}, is it your email? Email - {email}")
+print(f"Hello {name}!")
+
+def checkMail():
+    email = input("Enter your email : ")
+    print(f"Pls check your email : {email}")
+    checkmail = input("Right? (y/n) : ")
+    return checkmail
+
+if age >= 18: 
+    print('You are adult. You can save money in our bank')
+
+    cond = True
+    while(cond==True):
+        a = checkMail()
+        if a == 'y' or a == 'Y': 
+            cond = False
+        elif a == 'n' or a == 'N':
+            continue
+        else: 
+            print("Wrong input, pls enter only 'y' or 'n'.")
+            checkMail()
+        print("Congratulation....Well Done!")
+
+
+else: 
+    print("Sorry, you cann't save your money because you are too young")
+
+
+
